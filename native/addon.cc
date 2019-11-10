b9fc45019fde2afb1e21e835f5137ce33c810ca8
Christoph Purrer <chpurrer@fb.com>
2019-11-09 17:54:30 -0800

    Remove global variables from the Windows addon.cc

--- native/addon.cc
+++ native/addon.cc
@@ -1,22 +1,21 @@
 #include "napi_utils.h"
 #define WIN32_LEAN_AND_MEAN
 #define UNICODE
-#include <algorithm>
+#include <atomic>
 #include <iostream>
-#include <map>
 #include <string>
 #include <thread>
-#include <vector>
 #include <windows.h>
 
-struct NativeWindow;
-std::vector<std::thread> windowThreads;
-std::map<int, std::thread::id> windowIdToTheadId;
-int windowCounter = 0;
+namespace native
+{
 
-struct NativeWindow {
-	static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
-		switch (message) {
+struct NativeWindow
+{
+	static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+	{
+		switch (message)
+		{
 		case WM_DESTROY:
 			PostQuitMessage(0);
 			break;
@@ -27,12 +26,13 @@ struct NativeWindow {
 		return 0;
 	}
 
-	NativeWindow() : id_(++windowCounter) {
+	NativeWindow()
+	{
 		std::cout << "NativeWindow" << std::endl;
 		HINSTANCE hInstance = (HINSTANCE)GetModuleHandle(NULL);
 
 		// Register the windows class with unique className to allow multiWindow support
-		std::wstring windowClassName = L"Messenger" + std::to_wstring(id_);
+		std::wstring windowClassName = L"Messenger" + std::to_wstring(windowCounter_++);
 		WNDCLASSEX wcex;
 		wcex.cbSize = sizeof(WNDCLASSEX);
 		wcex.style = CS_HREDRAW | CS_VREDRAW;
@@ -47,80 +47,71 @@ struct NativeWindow {
 		wcex.lpszClassName = windowClassName.c_str();
 		wcex.hIconSm = LoadIcon(wcex.hInstance, IDI_APPLICATION);
 
-		if (!RegisterClassEx(&wcex)) {
+		if (!RegisterClassEx(&wcex))
+		{
 			std::cout << "Call to RegisterClassEx failed!" << std::endl;
 			return;
 		}
 
 		// Store instance handle in our global variable
-		hWnd_ = CreateWindow(windowClassName.c_str(),
-			L"NativeWindow",
-			WS_OVERLAPPEDWINDOW,
-			CW_USEDEFAULT,
-			CW_USEDEFAULT,
-			360,
-			640,
-			NULL,
-			NULL,
-			hInstance,
-			NULL);
+		HWND hWnd_ = CreateWindow(windowClassName.c_str(),
+								  L"NativeWindow",
+								  WS_OVERLAPPEDWINDOW,
+								  CW_USEDEFAULT,
+								  CW_USEDEFAULT,
+								  360,
+								  640,
+								  NULL,
+								  NULL,
+								  hInstance,
+								  NULL);
 
-		if (!hWnd_) {
+		if (!hWnd_)
+		{
 			std::cout << "Call to CreateWindow failed!" << std::endl;
 			return;
 		}
 
 		ShowWindow(hWnd_, SW_SHOW);
 		UpdateWindow(hWnd_);
-	};
 
-	~NativeWindow() {
-		std::cout << "~NativeWindow" << std::endl;
-		windowThreads.erase(
-			std::remove_if(windowThreads.begin(),
-				windowThreads.end(),
-				[&](const auto& t) { return t.get_id() == windowIdToTheadId.at(id_); }),
-			windowThreads.end());
-		std::cout << "windowThreads.size()" << windowThreads.size() << std::endl;
-	}
-
-	void run() {
-		if (hWnd_) {
-			// start main message loop
+		// start main message loop
+		if (hWnd_)
+		{
 			MSG msg;
-			while (GetMessage(&msg, NULL, 0, 0)) {
+			while (GetMessage(&msg, NULL, 0, 0))
+			{
 				TranslateMessage(&msg);
 				DispatchMessage(&msg);
 			}
 		}
-	}
+	};
 
-	int getId() {
-		return id_;
+	~NativeWindow()
+	{
+		std::cout << "~NativeWindow" << std::endl;
 	}
 
 private:
-	int id_;
-	HWND hWnd_;
+	static std::atomic<int> windowCounter_;
 };
+std::atomic<int> NativeWindow::windowCounter_{0};
 
-namespace native {
-	napi_value OpenNativeWindow(napi_env env, napi_callback_info info) {
-		windowThreads.push_back(std::thread([]() {
-			NativeWindow nativeWindow;
-			windowIdToTheadId.insert({ nativeWindow.getId(), std::this_thread::get_id() });
-			nativeWindow.run();
-		}));
-		return nullptr;
-	}
+napi_value OpenNativeWindow(napi_env env, napi_callback_info info)
+{
+	// TODO: Clean up nativeWindow threads
+	new std::thread(([]() { NativeWindow nativeWindow; }));
+	return nullptr;
+}
 
-	napi_value Initialize(napi_env env, napi_value exports) {
-		napi_value openNativeWindow;
-		NAPI_CHECK(napi_create_function(env, NULL, 0, OpenNativeWindow, NULL, &openNativeWindow));
-		NAPI_CHECK(napi_set_named_property(env, exports, "openNativeWindow", openNativeWindow));
-		return exports;
-	}
+napi_value Initialize(napi_env env, napi_value exports)
+{
+	napi_value openNativeWindow;
+	NAPI_CHECK(napi_create_function(env, NULL, 0, OpenNativeWindow, NULL, &openNativeWindow));
+	NAPI_CHECK(napi_set_named_property(env, exports, "openNativeWindow", openNativeWindow));
+	return exports;
+}
 
-	NAPI_MODULE(NODE_GYP_MODULE_NAME, Initialize)
+NAPI_MODULE(NODE_GYP_MODULE_NAME, Initialize)
 
 } // namespace native
